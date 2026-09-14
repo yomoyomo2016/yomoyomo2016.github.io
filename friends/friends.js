177087a352aa5dd4047d22d765844c7219d60e98
你的GitHub帳號名稱yomoyomo2016 <happy16168@gmail.com>
2026-09-15 07:00:28 +0800

    Site updated: 2026-09-15 07:00:24

---
+++ friends/friends.js
@@ -0,0 +1,25 @@
+const friendsData = {
+  friends: [
+    {
+      class: {
+        name: '傳送毛毯',
+        desc: '常常拜訪的友站～',
+        random: false // 設為 false 會依照先後順序顯示；設為 true 則每次整理隨機排列
+      },
+      list: [
+        {
+          name: '資工小廢物 - JN',
+          url: 'https://blog.giveanornot.com/',
+          avatar: 'https://blog.giveanornot.com/favicon.ico', // 若對方有專屬頭像網址可直接替換
+          desc: '資工系畢業，但其實沒有很會寫程式'
+        },
+        {
+          name: '來自月球的 L | 納莉 Anabel | Substack',
+          url: 'https://anabel.substack.com/', // 請確認並填入納莉完整的 Substack 網址
+          avatar: 'https://substack.com/favicon.ico', // 若有納莉的頭像圖片連結可替換於此
+          desc: '正職是考生的業餘小說作者，希望務正業的時間可以多一點。'
+        }
+      ]
+    }
+  ]
+};
