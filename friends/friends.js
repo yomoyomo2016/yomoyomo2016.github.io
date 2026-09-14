69003f9ebb7bfff65ff1c2e941a297128b096840
你的GitHub帳號名稱yomoyomo2016 <happy16168@gmail.com>
2026-09-15 07:23:22 +0800

    Site updated: 2026-09-15 07:23:21

--- friends/friends.js
+++ friends/friends.js
@@ -11,7 +11,7 @@ const friendsData = {
           name: '資工小廢物 - JN',
           url: 'https://blog.giveanornot.com/',
           // 使用 GitHub 提供的頭像服務（以 JN 的 GitHub 帳號為例，可穩定讀取）
-          avatar: 'https://github.com/GiveAnOrNot.png', 
+          avatar: 'https://blog.giveanornot.com/img/avatar.png', 
           desc: '資工系畢業，但其實沒有很會寫程式'
         },
         {
