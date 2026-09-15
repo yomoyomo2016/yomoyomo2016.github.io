e71f90e4dba8bbae3a85b3277198d691d50ea5b6
你的GitHub帳號名稱yomoyomo2016 <happy16168@gmail.com>
2026-09-15 15:07:21 +0800

    Site updated: 2026-09-15 15:07:20

--- friends/friends.js
+++ friends/friends.js
@@ -8,6 +8,13 @@ const friendsData = {
       },
       list: [
         {
+          name: 'MEOW-小橘貓',
+          url: 'https://meow.jumaoo.top/',
+          // 使用 GitHub 提供的頭像服務（以 JN 的 GitHub 帳號為例，可穩定讀取）
+          avatar: 'https://q1.qlogo.cn/g?b=qq&nk=763608788&s=640', 
+          desc: '本部落格使用的主題'
+        },
+        {
           name: '資工小廢物 - JN',
           url: 'https://blog.giveanornot.com/',
           // 使用 GitHub 提供的頭像服務（以 JN 的 GitHub 帳號為例，可穩定讀取）
@@ -16,7 +23,7 @@ const friendsData = {
         },
         {
           name: '來自月球的 L ',
-          url: 'https://anabel.substack.com/',
+          url: 'https://bittenbynellie.substack.com/',
           // 使用 Substack 平台的標準圖示
           avatar: 'https://substack.com/favicon.ico',
           desc: '正職是考生的業餘小說作者'
