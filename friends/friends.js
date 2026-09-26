3b3fcb8874a52f494c2c9322a9c65aa263051834
你的GitHub帳號名稱yomoyomo2016 <happy16168@gmail.com>
2026-09-26 18:55:41 +0800

    Site updated: 2026-09-26 18:55:39

--- friends/friends.js
+++ friends/friends.js
@@ -2,7 +2,7 @@ const friendsData = {
   friends: [
     {
       class: {
-        name: '永遠の天使🎀🪽', // 改成分類別名，避免跟頁面大標題「傳送毛毯」重複撞名
+        name: '永遠の天使🎀🪽',
         desc: '推し♡',
         random: false
       },
@@ -10,65 +10,57 @@ const friendsData = {
         {
           name: '萌留みみん/Moeru Mimin',
           url: 'https://www.youtube.com/@MoeruMimin',
-          // 使用 GitHub 提供的頭像服務（以 JN 的 GitHub 帳號為例，可穩定讀取）
-          avatar: 'https://yt3.googleusercontent.com/sxd1f1GZuFZ9f-hrSa4c5GcHoDxJw5FJO6bk2EXU_veoboNvZrqlx5Ww9OzOIUQXr7_gO-wVDg=s160-c-k-c0x00ffffff-no-rj', 
+          avatar: 'https://yt3.googleusercontent.com/sxd1f1GZuFZ9f-hrSa4c5GcHoDxJw5FJO6bk2EXU_veoboNvZrqlx5Ww9OzOIUQXr7_gO-wVDg=s160-c-k-c0x00ffffff-no-rj',
           desc: '現代社会に潰されるマスターの心を支えにきました⋯♡🪽'
         }
       ]
     },
     {
       class: {
-        name: '常常拜訪&推薦ㄉ友站～', // 改成分類別名，避免跟頁面大標題「傳送毛毯」重複撞名
+        name: '常常拜訪&推薦ㄉ友站～',
         desc: '傳送',
         random: true
-      }
+      },
       list: [
         {
           name: 'MEOW-小橘貓',
           url: 'https://meow.jumaoo.top/',
-          // 使用 GitHub 提供的頭像服務（以 JN 的 GitHub 帳號為例，可穩定讀取）
-          avatar: 'https://q1.qlogo.cn/g?b=qq&nk=763608788&s=640', 
+          avatar: 'https://q1.qlogo.cn/g?b=qq&nk=763608788&s=640',
           desc: '本部落格使用的主題'
         },
         {
           name: '資工小廢物 - JN',
           url: 'https://blog.giveanornot.com/',
-          // 使用 GitHub 提供的頭像服務（以 JN 的 GitHub 帳號為例，可穩定讀取）
-          avatar: 'https://blog.giveanornot.com/img/avatar.png', 
+          avatar: 'https://blog.giveanornot.com/img/avatar.png',
           desc: '資工系畢業，但其實沒有很會寫程式'
         },
         {
           name: '來自月球的 L ',
           url: 'https://bittenbynellie.substack.com/',
-          // 使用 Substack 平台的標準圖示
           avatar: 'https://substack.com/favicon.ico',
           desc: '正職是考生的業餘小說作者'
         },
         {
           name: '嶺上苓的小屋',
           url: 'https://lingonhills.github.io/',
-          // 
-          avatar: 'https://scontent-tpe5-1.cdninstagram.com/v/t51.82787-19/751001219_18046754444633267_3441101762396552935_n.jpg?efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4zMjAuYzIifQ&_nc_ht=scontent-tpe5-1.cdninstagram.com&_nc_cat=111&_nc_oc=Q6cZ2gHCozYPFOuvQJLEmiJ6UjDSEv9LwXKaPOHB2y5vrPh8tURuFS0mGgo-avSktT-j8Dw&_nc_ohc=xygwuAUET40Q7kNvwFx5CGl&_nc_gid=GuWYrT2cfkKbEPJ8w2gn-w&edm=APs17CUBAAAA&ccb=7-5&oh=00_AQLcqloul234eGK0IQZ4cj-NQ3f-AdyLDDMo1coxYvbdxQ&oe=6ABD639F&_nc_sid=10d13b',
+          avatar: 'https://lingonhills.github.io/favicon.ico', // Instagram CDN 連結通常帶有時效防盜鏈，若破圖建議改用 favicon
           desc: '嗨，我是嶺上苓，我有在畫一些漫畫。'
         },
         {
           name: 'NudoBlog - 小透明龍女 ',
           url: 'https://blog.nudoragon.com/',
-          // 
           avatar: 'https://blog.nudoragon.com/static/(pic)(2)2560px-Transgender_Pride_flag.svg.png',
           desc: ''
         },
         {
           name: '百合坂',
           url: 'https://yurizaka.com/',
-          // 
           avatar: 'https://yurizaka.com/logo.png',
           desc: '百合作品資料庫'
         },
         {
-          name: '水影獨舞在永恆 - lazurloner’s Blog',
+          name: "水影獨舞在永恆 - lazurloner's Blog",
           url: 'https://lazur.me/',
-          // 
           avatar: 'https://lazur.me/wp-content/uploads/2025/08/20250731-website-1024x1024-1.jpg',
           desc: 'taiwan.wtf 站長，資安工程師、性別運動者、劇場創作者'
         }
